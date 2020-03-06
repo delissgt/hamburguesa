59d311379efb91d6cc679e8018588e4ce8c482cc
delissgt <delissgt@gmail.com>
2020-03-05 18:49:41 -0600

    163 Connecting State to Build Controls, now is posible to add ingredients with controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import  BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.5,
+    bacon: 0.7
+};
+
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -9,7 +18,21 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
+    };
+
+    addIngredientHandler = (type) => {
+      const oldCount = this.state.ingredients[type];
+      const updatedCount = oldCount + 1;
+      const updateIngredients = {
+          ...this.state.ingredients
+      };
+      updateIngredients[type] = updatedCount;
+      const priceAddition = INGREDIENT_PRICES[type];
+      const oldPrice = this.state.totalPrice;
+      const newPrice = oldPrice + priceAddition;
+      this.setState({totalPrice: newPrice, ingredients:updateIngredients})
     };
 
     render () {
@@ -18,7 +41,9 @@ class BurgerBuilder extends Component {
                {/*<div>Burger</div>*/}
                <Burger ingredients={this.state.ingredients}/>
                {/*<div>Build Controls</div>*/}
-               <BuildControls/>
+               <BuildControls
+                ingredientAdded={this.addIngredientHandler}
+               />
             </Aux>
         );
     }
